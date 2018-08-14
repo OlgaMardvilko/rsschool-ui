deaef80d12f5b1e688c2902247afa123b08fb945
anastasia <novakanastasya@yandex.ru>
2018-08-14 23:27:16 +0300

    refactor: add university at education.ts

--- src/app/reference-data/education.ts
+++ src/app/reference-data/education.ts
@@ -158,48 +158,394 @@ export const UNIVERSITIES = [
                 name: 'Marketing, Management and Entrepreneurship Faculty',
             },
             {
-                id: 'bntu-faculty-1',
+                id: 'bntu-faculty-6',
                 name: 'Power Engineering Faculty',
             },
             {
-                id: 'bntu-faculty-6',
+                id: 'bntu-faculty-7',
                 name: 'Information Technologies and Robotics Faculty',
             },
             {
-                id: 'bntu-faculty-7',
+                id: 'bntu-faculty-8',
                 name: 'Management Technologies and Humanitarization Faculty',
             },
             {
-                id: 'bntu-faculty-8',
+                id: 'bntu-faculty-9',
                 name: 'Engineering and Pedagogy Faculty',
             },
             {
-                id: 'bntu-faculty-9',
+                id: 'bntu-faculty-10',
                 name: 'Power Plant Construction and Engineering Services Faculty',
             },
             {
-                id: 'bntu-faculty-10',
+                id: 'bntu-faculty-11',
                 name: 'Architectural Faculty',
             },
             {
-                id: 'bntu-faculty-11',
+                id: 'bntu-faculty-12',
                 name: 'Civil Engineering Faculty',
             },
             {
-                id: 'bntu-faculty-12',
+                id: 'bntu-faculty-13',
                 name: 'Instrumentation Engineering Faculty',
             },
             {
-                id: 'bntu-faculty-13',
+                id: 'bntu-faculty-14',
                 name: 'Transport Communications Faculty',
             },
             {
-                id: 'bntu-faculty-14',
+                id: 'bntu-faculty-15',
                 name: 'Military and Technical Faculty',
             },
             {
-                id: 'bntu-faculty-15',
+                id: 'bntu-faculty-16',
                 name: 'Sports and Technical Faculty',
+            },
+        ],
+    },
+    {
+        id: 'bseu',
+        name: 'Belarusian State Economic University',
+        faculties: [
+            {
+                id: 'bseu-faculty-1',
+                name: 'Faculty of Management',
+            },
+            {
+                id: 'bseu-faculty-2',
+                name: 'Faculty of Banking and Finance',
+            },
+            {
+                id: 'bseu-faculty-3',
+                name: 'Higher School of Tourism',
+            },
+            {
+                id: 'bseu-faculty-4',
+                name: 'Faculty of Accounting and Economics',
+            },
+        ],
+    },
+    {
+        id: 'mslu',
+        name: 'Minsk State Linguistic University',
+        faculties: [
+            {
+                id: 'mslu-faculty-1',
+                name: 'Department of Intercultural Communication',
+            },
+        ],
+    },
+    {
+        id: 'bsatu',
+        name: 'Belarusian State Agrarian Technical University',
+        faculties: [
+            {
+                id: 'bsatu-faculty-1',
+                name: 'Agromechanical faculty',
+            },
+            {
+                id: 'bsatu-faculty-2',
+                name: 'Agro-power faculty',
+            },
+            {
+                id: 'bsatu-faculty-3',
+                name: 'Farm machinery service faculty',
+            },
+            {
+                id: 'bsatu-faculty-4',
+                name: 'Engineering and Technology faculty',
+            },
+            {
+                id: 'bsatu-faculty-5',
+                name: 'Business and management faculty',
+            },
+        ],
+    },
+    {
+        id: 'bspc',
+        name: 'Brest State Polytechnic College',
+        faculties: [
+            {
+                id: 'bspc-faculty-1',
+                name: 'Building department',
+            },
+        ],
+    },
+    {
+        id: 'bstu',
+        name: 'Bryansk State Technical University',
+        faculties: [
+            {
+                id: 'bstu-faculty-1',
+                name: 'Building department',
+            },
+            {
+                id: 'bstu-faculty-2',
+                name: 'Department of Energy and Electronics',
+            },
+        ],
+    },
+    {
+        id: 'brstu',
+        name: 'Brest State Technical University',
+        faculties: [
+            {
+                id: 'bstu-faculty-1',
+                name: 'Faculty of Civil Engineering',
+            },
+            {
+                id: 'bstu-faculty-2',
+                name: 'Faculty of Electronic-Informational Systems',
+            },
+        ],
+    },
+    {
+        id: 'uw',
+        name: 'University of Warsaw',
+        faculties: [
+            {
+                id: 'uw-faculty-1',
+                name: 'Faculty of Economic Sciences',
+            },
+        ],
+    },
+    {
+        id: 'iit_bsuir',
+        name: 'Institute of Information Technologies BSUIR',
+        faculties: [
+            {
+                id: 'iit_bsuir-faculty-1',
+                name: 'Faculty of Computer Technologies',
+            },
+            {
+                id: 'iit_bsuir-faculty-2',
+                name: 'Faculty of Professional Development and Retraining',
+            },
+        ],
+    },
+    {
+        id: 'pime',
+        name: 'Private Institute of Management and Entrepreneurship',
+        faculties: [
+            {
+                id: 'pime-faculty-1',
+                name: 'Law Faculty',
+            },
+            {
+                id: 'pime-faculty-2',
+                name: 'Economic Faculty',
+            },
+        ],
+    },
+    {
+        id: 'fsgsu',
+        name: 'Francisk Skorina Gomel State University',
+        faculties: [
+            {
+                id: 'fsgsu-faculty-1',
+                name: 'Faculty of Mathematics and programming technologies',
+            },
+        ],
+    },
+    {
+        id: 'psstug',
+        name: 'Pavel Sukhoi State Technical University of Gomel',
+        faculties: [
+            {
+                id: 'psstug-faculty-1',
+                name: 'Faculty of Automation and Information Systems',
+            },
+        ],
+    },
+    {
+        id: 'yksug',
+        name: 'Yanka Kupala State University of Grodno',
+        faculties: [
+            {
+                id: 'yksug-faculty-1',
+                name: 'Faculty of Mathematics and Information Science',
+            },
+        ],
+    },
+    {
+        id: 'psu',
+        name: 'Polotsk State University',
+        faculties: [
+            {
+                id: 'psu-faculty-1',
+                name: 'Faculty of Information Technologies',
+            },
+            {
+                id: 'psu-faculty-2',
+                name: 'Faculty of Civil Engineering',
+            },
+        ],
+    },
+    {
+        id: 'bstu',
+        name: 'Belarusian State Technological University',
+        faculties: [
+            {
+                id: 'bstu-faculty-1',
+                name: 'Chemical Technology and Engineering Faculty',
+            },
+            {
+                id: 'bstu-faculty-2',
+                name: 'Faculty of Print Technology and Media Communications',
+            },
+            {
+                id: 'bstu-faculty-3',
+                name: 'Faculty of Information Technology',
+            },
+        ],
+    },
+    {
+        id: 'bspu',
+        name: 'Belarusian State Pedagogical University Named After Maxim Tank',
+        faculties: [
+            {
+                id: 'bspu-faculty-1',
+                name: 'Department of History',
+            },
+            {
+                id: 'bspu-faculty-2',
+                name: 'Faculty of Physics and Mathematics Department',
+            },
+        ],
+    },
+    {
+        id: 'cbil',
+        name: 'College of Business and Law',
+        faculties: [
+            {
+                id: 'cbil-faculty-1',
+                name: 'Information Technology Software',
+            },
+        ],
+    },
+    {
+        id: 'belsut',
+        name: 'Belarusian State University of Transport',
+        faculties: [
+            {
+                id: 'belsut-faculty-1',
+                name: 'Faculty of Humanities and Economics',
+            },
+            {
+                id: 'belsut-faculty-2',
+                name: 'Faculty "Transportation process management"',
+            },
+        ],
+    },
+    {
+        id: 'iea',
+        name: 'Institute of Entrepreneurial Activity',
+        faculties: [
+            {
+                id: 'iea-faculty-1',
+                name: 'Faculty of Economics and Business',
+            },
+        ],
+    },
+    {
+        id: 'bsat',
+        name: 'The Belarusian State Academy of Telecommunications',
+        faculties: [
+            {
+                id: 'bsat-faculty-1',
+                name: 'Faculty of Engineering and Communication Technology',
+            },
+        ],
+    },
+    {
+        id: 'bsmu',
+        name: 'Belarusian State Medical University',
+        faculties: [
+            {
+                id: 'bsmu-faculty-1',
+                name: 'Faculty of General Medicine',
+            },
+        ],
+    },
+    {
+        id: 'mesi',
+        name: 'Moscow State University of Economics, Statistics, and Informatics',
+        faculties: [
+            {
+                id: 'mesi-faculty-1',
+                name: 'Institute of Management',
+            },
+        ],
+    },
+    {
+        id: 'vsu',
+        name: 'Vitebsk State University',
+        faculties: [
+            {
+                id: 'vsu-faculty-1',
+                name: 'Faculty of Social Pedagogics and Psychology',
+            },
+            {
+                id: 'vsu-faculty-2',
+                name: 'Faculty of Mathematics and IT',
+            },
+            {
+                id: 'vsu-faculty-3',
+                name: 'Faculty of Physical Culture and Sports',
+            },
+        ],
+    },
+    {
+        id: 'mstc',
+        name: 'Minsk State Technological College',
+        faculties: [
+            {
+                id: 'mstc-faculty-1',
+                name: 'Photography',
+            },
+        ],
+    },
+    {
+        id: 'shrick',
+        name: "Shirokov's Institute of Contemporary Knowledge",
+        faculties: [
+            {
+                id: 'shrick-faculty-1',
+                name: 'Faculty of Arts',
+            },
+            {
+                id: 'shrick-faculty-2',
+                name: 'Humanitarian Faculty',
+            },
+        ],
+    },
+    {
+        id: 'msce',
+        name: 'Minsk State College of Electronics ',
+        faculties: [
+            {
+                id: 'msce-faculty-1',
+                name: 'Information Technology Software',
+            },
+        ],
+    },
+    {
+        id: 'apa',
+        name: 'Academy of Public Administration under the aegis of the President of the Republic of Belarus',
+        faculties: [
+            {
+                id: 'apa-faculty-1',
+                name: 'Faculty of Management',
+            },
+        ],
+    },
+    {
+        id: 'MITSO',
+        name: 'International University «MITSO»',
+        faculties: [
+            {
+                id: 'MITSO-faculty-1',
+                name: 'Faculty of International Economic Relations and Management',
             },
         ],
     },
