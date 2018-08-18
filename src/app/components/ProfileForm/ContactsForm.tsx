80936f730cab138b8c9c6a2876d47766f6b0d2df
anastasia <novakanastasya@yandex.ru>
2018-08-19 01:27:56 +0300

    refactor: add validate attribute to ContactsForm

--- src/app/components/ProfileForm/ContactsForm.tsx
+++ src/app/components/ProfileForm/ContactsForm.tsx
@@ -2,7 +2,7 @@ import ReduxFormInput from 'components/ReduxFormInput';
 import * as React from 'react';
 import { FormGroup, InputGroup, InputGroupAddon, Label } from 'reactstrap';
 import { Field } from 'redux-form';
-import { emailFormatter, emailParser } from './parsers-formatters';
+import { emailFormatter, emailParser, required } from './parsers-formatters';
 
 class ContactsForm extends React.PureComponent<any, any> {
     render() {
@@ -18,35 +18,41 @@ class ContactsForm extends React.PureComponent<any, any> {
                             placeholder="111-111-11-11"
                             component={ReduxFormInput}
                             type="tel"
+                            validate={[required]}
                         />
                     </InputGroup>
                 </FormGroup>
             </div>,
+
             <div key="1" className="row">
                 <FormGroup className="col-md-12">
                     <Label className="field-required">Email</Label>
                     <Field
                         component={ReduxFormInput}
                         format={emailFormatter}
-                        name="contacts.emails"
+                        name="emails"
                         parse={emailParser}
                         placeholder="Email"
                         type="email"
+                        validate={[required]}
                     />
                 </FormGroup>
             </div>,
+
             <div key="2" className="row">
                 <FormGroup className="col-md-12">
                     <Label>You nickname at Skype</Label>
                     <Field component={ReduxFormInput} name="contacts.skype" placeholder="ivan.ivanov" type="text" />
                 </FormGroup>
             </div>,
+
             <div key="3" className="row">
                 <FormGroup className="col-md-12">
                     <Label>You nickname at Telegram</Label>
                     <Field component={ReduxFormInput} name="contacts.telegram" placeholder="@ivan.ivanov" type="text" />
                 </FormGroup>
             </div>,
+
             <div key="4" className="row">
                 <FormGroup className="col-md-12">
                     <Label>Other contact</Label>
