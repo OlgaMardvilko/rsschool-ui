9fce41a2b1112ba15a3a3ddb766de15dca35dfb3
anastasia <novakanastasya@yandex.ru>
2018-08-19 03:04:49 +0300

    refactor: add heading

--- src/app/components/ProfileForm/index.tsx
+++ src/app/components/ProfileForm/index.tsx
@@ -10,40 +10,47 @@ import EmploymentForm from './EmploymentForm';
 const ProfileForm = (props: InjectedFormProps) => {
     const { handleSubmit, pristine, submitting } = props;
     return (
-        <form onSubmit={handleSubmit}>
-            <h5>PERSONAL INFORMATION</h5>
-            <Jumbotron>
-                <NamesForm />
-            </Jumbotron>
+        <div>
+            <h2>Student Profile</h2>
+            <p className="form-text text-muted">
+                Please, provide valid info. It's needed to our HRs to get in touch with you.
+            </p>
 
-            <h5>CONTACTS</h5>
-            <Jumbotron>
-                <ContactsForm />
-            </Jumbotron>
+            <form onSubmit={handleSubmit} className="mt-5">
+                <h5>PERSONAL INFORMATION</h5>
+                <Jumbotron>
+                    <NamesForm />
+                </Jumbotron>
 
-            <h5>EDUCATION</h5>
-            <Jumbotron>
-                <EducationForm />
-            </Jumbotron>
+                <h5>CONTACTS</h5>
+                <Jumbotron>
+                    <ContactsForm />
+                </Jumbotron>
 
-            <h5>EMPLOYMENT HISTORY</h5>
-            <Jumbotron>
-                <EmploymentForm />
-            </Jumbotron>
+                <h5>EDUCATION</h5>
+                <Jumbotron>
+                    <EducationForm />
+                </Jumbotron>
 
-            <h5>OTHER</h5>
-            <Jumbotron>
-                <OtherForm />
-            </Jumbotron>
+                <h5>EMPLOYMENT HISTORY</h5>
+                <Jumbotron>
+                    <EmploymentForm />
+                </Jumbotron>
 
-            <div className="row text-center">
-                <div className="form-group col-md-12">
-                    <Button color="success" type="submit" disabled={pristine || submitting}>
-                        Save Information
-                    </Button>
+                <h5>OTHER</h5>
+                <Jumbotron>
+                    <OtherForm />
+                </Jumbotron>
+
+                <div className="row text-center">
+                    <div className="form-group col-md-12">
+                        <Button color="success" type="submit" disabled={pristine || submitting}>
+                            Save Information
+                        </Button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 };
 
