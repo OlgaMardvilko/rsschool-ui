a3668962d2f68591a1ba45c649e28c1d7120589a
vitol <omardvilko@mail.ru>
2018-09-06 09:33:02 +0300

    refactor: remove unnecessary return in selectors

--- src/app/core/selectors/index.ts
+++ src/app/core/selectors/index.ts
@@ -4,21 +4,18 @@ export const getUserProfile = (state: RootState) => {
     if (state.user && state.user.profile) {
         return state.user.profile;
     }
-    return;
 };
 
 export const getStudentMentors = (state: RootState) => {
     if (state.student.student) {
         return state.student.student.mentors;
     }
-    return;
 };
 
 export const getUserRole = (state: RootState) => {
     if (state.user.participations && state.user.participations[0] && state.user.participations[0].role) {
         return state.user.participations[0].role;
     }
-    return;
 };
 
 export function isAnyPartLoaded({ router, ...state }: any) {
